dbb668cf491ff26550f37a6d5887ffab6163d3a3
MINT <33680799+kyu1204@users.noreply.github.com>
2023-11-15 18:23:11 +0900

    ✨ Feat: docker setup (#4)
    #
    
    Co-authored-by: 김민규 <minkyu.kim@birdview.kr>

--- sara_server/settings.py
+++ sara_server/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+ENV = os.environ.get("MODE", "DEV")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "questions",
+    "status",
 ]
 
 MIDDLEWARE = [
@@ -71,16 +74,29 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "sara_server.wsgi.application"
 
+APPEND_SLASH = False
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if ENV == "DEV":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ["DB_NAME"],
+            "HOST": os.environ["DB_HOST"],
+            "PORT": os.environ["DB_PORT"],
+            "USER": os.environ["DB_USER"],
+            "PASSWORD": os.environ["DB_PASSWORD"],
+        }
+    }
 
 
 # Password validation
