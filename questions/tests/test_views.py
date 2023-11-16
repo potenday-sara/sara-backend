933ccd53ebea3456c9b30b4f02e7dcbe40b15072
MINT <33680799+kyu1204@users.noreply.github.com>
2023-11-16 13:52:14 +0900

    Feature/question admin (#7)
    * ✨ Feat: question feedback, admin 설정 추가
    
    #
    
    * ✨ Feat: 테스트코드 추가
    
    #

--- questions/tests/test_views.py
+++ questions/tests/test_views.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 
+from questions.models import QuestionFeedback
 from questions.tests.factories import QuestionFactory
 
 
-class QuestionsView(TestCase):
+class QuestionsView_테스트(TestCase):
     client_class = APIClient
 
     @classmethod
@@ -19,3 +20,13 @@ class QuestionsView(TestCase):
             self.assertEqual(response.status_code, 200)
         with self.subTest("질문 목록이 리턴된다."):
             self.assertEqual(len(response.data), len(self.question_list))
+
+    def test_questions_feedback_요청_성공_시(self):
+        response = self.client.post(
+            self.url + f"{self.question_list[0].id}" + "/feedback/",
+            data={"feedback": "1", "question": self.question_list[0].id},
+        )
+        with self.subTest("status code 201이 리턴된다."):
+            self.assertEqual(response.status_code, 201)
+        with self.subTest("questions feedback 데이터가 생성된다"):
+            QuestionFeedback.objects.get(question=self.question_list[0], feedback="1")
