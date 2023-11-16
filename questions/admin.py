dd44485a4bf31c352d0d344594be6f85d2967877
MINT <pb1123love@gmail.com>
2023-11-16 13:43:56 +0900

    ✨ Feat: question feedback, admin 설정 추가
    #

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from questions.models import Answer, Question, QuestionFeedback
+
+
+class AnswerInline(admin.StackedInline):
+    fields = ["content"]
+    model = Answer
+    extra = 1
+
+
+class QuestionFeedbackInline(admin.StackedInline):
+    fields = ["feedback"]
+    model = QuestionFeedback
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["type", "content", "product"]
+    list_display = [
+        "type",
+        "content",
+        "product",
+        "created_at",
+        "updated_at",
+    ]
+    search_fields = ["content", "product"]
+    list_display_links = ["content", "product"]
+    readonly_fields = ["created_at", "updated_at"]
+    inlines = [AnswerInline, QuestionFeedbackInline]
+
+
+admin.site.register(Question, QuestionAdmin)
