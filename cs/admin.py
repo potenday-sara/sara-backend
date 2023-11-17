b335a4b3e484ec74fff25ac6eb2f49652e5ec145
MINT <pb1123love@gmail.com>
2023-11-17 19:27:19 +0900

    🔨 Fix: cs 어드민 등록
    #

--- cs/admin.py
+++ cs/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from cs.models import Feedback
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    fields = ["content"]
+    list_display = [
+        "content",
+        "created_at",
+        "updated_at",
+    ]
+    search_fields = ["content"]
+    list_display_links = ["content"]
+    readonly_fields = ["created_at", "updated_at"]
+
+
+admin.site.register(Feedback, FeedbackAdmin)
