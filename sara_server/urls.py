ffabef0e0e20387161fe440ce2d083aae9c726e4
MINT <pb1123love@gmail.com>
2023-11-17 20:45:29 +0900

    🔨 Fix: swagger 정보 수정
    #

--- sara_server/urls.py
+++ sara_server/urls.py
@@ -26,9 +26,9 @@ schema_view = get_schema_view(
         default_version="v1",
         description="sara api",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        contact=openapi.Contact(email="potenday.sara@gmail.com"),
     ),
+    url="https://api.sara-mara.com",
     public=True,
     permission_classes=[
         permissions.AllowAny,
