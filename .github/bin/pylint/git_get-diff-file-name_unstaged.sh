6813e5010a7c1fb97957cd9227db89b1289adf2d
MINT <pb1123love@gmail.com>
2023-11-28 10:07:30 +0900

    🔨 Fix: lint 수정
    #

--- .github/bin/pylint/git_get-diff-file-name_unstaged.sh
+++ .github/bin/pylint/git_get-diff-file-name_unstaged.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-(sh ../bin/lint_common/git_get-diff-file-name_unstaged.sh) | grep -E '\.py$' | tr '\n' ' '
+(sh .github/bin/lint_common/git_get-diff-file-name_unstaged.sh) | grep -E '\.py$' | tr '\n' ' '
