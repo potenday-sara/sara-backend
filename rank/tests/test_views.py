dc4e6deebcadbb553b2c6e7cdb23ffa965637e77
MINT <pb1123love@gmail.com>
2023-11-29 10:35:37 +0900

    ✨ Feat: rank view 테스트코드 추가
    #

--- rank/tests/test_views.py
+++ rank/tests/test_views.py
@@ -12,11 +12,11 @@ class RankView_테스트(TestCase):
     def setUpTestData(cls):
         cls.url = "/rank/"
         cls.product_list = {"product1": 5, "product2": 3, "product3": 1}
-        for product, product_amount in cls.product_list.items():
+        for idx in range(1, 11):
             QuestionFactory.create_batch(
-                product_amount,
+                idx,
                 type="sara",
-                product=product,
+                product=f"product_{idx}",
             )
 
     def test_questions_조회_요청_성공_시(self):
@@ -27,7 +27,9 @@ class RankView_테스트(TestCase):
 
         with self.subTest("status code 200이 리턴된다."):
             self.assertEqual(response.status_code, 200)
+
         with self.subTest("랭킹 목록이 리턴된다."):
-            self.assertEqual(len(response.data), len(self.product_list))
-            product_rank = [rank["product"] for rank in response.data]
-            self.assertEqual(product_rank, list(self.product_list.keys()))
+            self.assertTrue(len(response.data) >= 1)
+
+        with self.subTest("최대 5개 까지만 리턴된다."):
+            self.assertEqual(len(response.data), 5)
