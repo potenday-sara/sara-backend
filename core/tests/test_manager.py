04caa2e8447923ac568b2b51ed0c8fc960302f28
MINT <pb1123love@gmail.com>
2023-11-30 22:54:28 +0900

    ✨ Feat: test corvarage 대응
    #

--- core/tests/test_manager.py
+++ core/tests/test_manager.py
@@ -33,6 +33,10 @@ class RestoreQuerySet_테스트(TestCase):
         self.obj2 = QuestionFactory()
         Question.objects.all().delete()
 
+    def test_hard_delete(self):
+        Question.all_objects.all().hard_delete()
+        self.assertEqual(Question.all_objects.all().deleted().count(), 0)
+
     def test_restore(self):
         Question.all_objects.all().deleted().restore()
         self.assertEqual(Question.all_objects.all().active().count(), 2)
@@ -48,6 +52,7 @@ class RestoreQuerySet_테스트(TestCase):
 class BaseManager_테스트(TestCase):
     def test_get_or_none(self):
         self.assertIsNotNone(Question.all_objects.get_or_none(pk=QuestionFactory().pk))
+        self.assertIsNone(Question.all_objects.get_or_none(pk=9999))
 
     def test_get_or_raise_not_found(self):
         with self.assertRaises(NotFound):
--- questions/views.py
+++ questions/views.py
@@ -18,6 +18,7 @@ class QuestionViewSet(
 ):
     queryset = Question.objects.select_related("answer").all()
     serializer_class = QuestionSerializer
+    pagination_class = None
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
@@ -30,11 +31,6 @@ class QuestionViewSet(
             .annotate(random=Random())
             .order_by("random")[:QUESTION_LIST_MAX_LENGTH]
         )
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
