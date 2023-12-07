dd883ee474419e0f085aea05f3acb6b557ef9506
MINT <pb1123love@gmail.com>
2023-12-07 16:11:51 +0900

    🔨 Fix: admin column 수정
    #

--- shop/admin.py
+++ shop/admin.py
@@ -4,11 +4,10 @@ from shop.models import Category
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    fields = ["id", "code", "name"]
-    list_display = ["id", "code", "name"]
+    fields = ["code", "name"]
+    list_display = ["code", "name"]
     search_fields = ["code", "name"]
     list_display_links = ["code", "name"]
-    readonly_fields = ["id"]
 
 
 admin.site.register(Category, CategoryAdmin)
