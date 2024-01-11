d454dfb36a37fec36d9d67986b76a7bdc3464389
MINT <pb1123love@gmail.com>
2024-01-12 00:02:32 +0900

    feat: cs feedback 컬럼 추가

--- questions/models/cs_models.py
+++ questions/models/cs_models.py
@@ -4,17 +4,25 @@ from core.models import CommonModel
 
 
 class QuestionFeedback(CommonModel):
-    question = models.ForeignKey("Question", on_delete=models.CASCADE)
-    feedback = models.IntegerField(choices=[(1, "good"), (0, "normal"), (-1, "bad")])
+    class FeedbackType(models.IntegerChoices):
+        GOOD = 1, "good"
+        NORMAL = 0, "normal"
+        BAD = -1, "bad"
+
+    question = models.OneToOneField("Question", on_delete=models.CASCADE)
+    feedback = models.IntegerField(choices=FeedbackType.choices)
 
     class Meta:
         db_table = "question_feedbacks"
         app_label = "questions"
         verbose_name = "질문 피드백"
 
+    def __str__(self):
+        return self.FeedbackType(self.feedback).label
+
 
 class Feedback(CommonModel):
-    question = models.ForeignKey("Question", on_delete=models.CASCADE, null=True)
+    question = models.OneToOneField("Question", on_delete=models.CASCADE, null=True)
     content = models.TextField()
 
     # 추후 유저 모델이 생기면 수정
