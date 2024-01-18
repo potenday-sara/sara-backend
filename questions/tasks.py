8d1ad25ebff1e66d872f8eea8204072e717f2a6d
MINT <pb1123love@gmail.com>
2024-01-18 19:01:04 +0900

    fix: lint

--- questions/tasks.py
+++ questions/tasks.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 
-from datetime import datetime
-
 from celery import shared_task
 from django.conf import settings
 from django.utils import timezone
