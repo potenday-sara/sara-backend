5df85c25a7279b3e392f76bb2841bde588279ba7
eunbin079 <tree0709@naver.com>
2024-06-27 23:11:10 +0900

    format

--- shop/views.py
+++ shop/views.py
@@ -7,7 +7,7 @@ from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
 from core.cache import RedisCache
 from shop.consts import SHOP_GOODS_LIST_TTL, SHOP_MAX_SEARCH_COUNT, SHOP_MAX_SEARCH_TTL
-from shop.models import Category
+from shop.models import Category, ShopType
 from shop.serializers import (
     CategoryGoodsSerializer,
     CategorySerializer,
@@ -15,7 +15,6 @@ from shop.serializers import (
     SearchRequestSerializer,
 )
 from shop.services import CoupangAPI
-from shop.models import ShopType
 
 
 class CategoryViewSet(ReadOnlyModelViewSet):
